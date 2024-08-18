8b328baf8f7ba15fbba98610bb00c3d478beb8a9
sameerkindia <sameerkhanaa5@gmail.com>
2024-08-18 18:29:01 +0530

    ready to deploy

--- script.js
+++ script.js
@@ -41,68 +41,69 @@ function locomotiveScrolltrigger() {
 function loadingAnimation() {
   let tl = gsap.timeline();
 
-  // tl.from(".line h2", {
-  //   y: 150,
-  //   stagger: 0.25,
-  //   // opacity: 0,
-  //   duration: 0.6,
-  //   delay: 0.5,
-  // });
+  tl.from(".line h2", {
+    y: 150,
+    stagger: 0.25,
+    // opacity: 0,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-  // tl.from("#line1-part1", {
-  //   opacity: 0,
-  //   onStart: function () {
-  //     let h2timer = document.querySelector("#line1-part1 h5");
-  //     let grow = 0;
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      let h2timer = document.querySelector("#line1-part1 h5");
+      let grow = 0;
 
-  //     const h2timeupdater = setInterval(() => {
-  //       grow++;
-  //       h2timer.textContent = grow;
+      const h2timeupdater = setInterval(() => {
+        grow++;
+        h2timer.textContent = grow;
 
-  //       if (grow === 100) {
-  //         clearInterval(h2timeupdater);
-  //       }
-  //     }, 30);
-  //   },
-  // });
+        if (grow === 100) {
+          clearInterval(h2timeupdater);
+        }
+      }, 30);
+    },
+  });
 
-  // tl.to(".line h3", {
-  //   animationName: "animeNow",
-  //   opacity: 1,
-  // });
-
-  // tl.to("#loader", {
-  //   opacity: 0,
-  //   duration: 0.2,
-  //   delay: 3,
-  // });
-
-  // tl.from("#page1", {
-  //   y: 1200,
-  //   opacity: 0,
-  //   delay: 0.2,
-  //   duration: 0.5,
-  //   // ease: power4,
-  // });
+  tl.to(".line h3", {
+    animationName: "animeNow",
+    opacity: 1,
+  });
 
   tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 3,
     display: "none",
   });
 
-  tl.from(".hero-heading", {
-    // y: 150,
-    y: 350,
-
-    stagger: 0.2,
+  tl.from("#page1", {
+    y: 1200,
+    opacity: 0,
+    delay: 0.2,
+    duration: 0.5,
+    // ease: power4,
   });
 
-  tl.from(
-    "#hero1 , #page2",
-    {
-      opacity: 0,
-    },
-    "-=1.2"
-  );
+  // tl.to("#loader", {
+  //   display: "none",
+  // });
+
+  // tl.from(".hero-heading", {
+  //   // y: 150,
+  //   y: 350,
+
+  //   stagger: 0.2,
+  // });
+
+  // tl.from(
+  //   "#hero1 , #page2",
+  //   {
+  //     opacity: 0,
+  //   },
+  //   "-=1.2"
+  // );
 }
 
 function cursurAnimation() {
